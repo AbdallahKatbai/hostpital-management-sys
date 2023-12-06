d0f916a21d66fe749dd3c6899768473b0976b042
Abdallah-Katbai <abdallah.katbai@gmail.com>
2023-12-06 04:58:21 +0200

    completed the sql tables

--- connection.php
+++ connection.php
@@ -2,9 +2,9 @@
 $host = "localhost";
 $db_user = "root";
 $db_pass = null;
-//$db_name = "foodex_db";
+$db_name = "hospital_management_db";
 
-$mysqli = new mysqli($host, $db_user, $db_pass/*, $db_name*/);
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 
 if ($mysqli -> connect_error){
     die("" . $mysqli -> connect_error);
